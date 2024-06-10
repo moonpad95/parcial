ab011553fa156b936e1627d60076ece9116f9137
Luis Luna <luishernandes2.jlh@gmail.com>
2024-06-10 13:56:28 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import './App.css';
-import Menu from './Menu.js';
-import Conceptos from './conceptos.js';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Menu from "./Menu.js";
+import Conceptos from "./conceptos.js";
+import Alumnos from "./alumnos.js";
+import Home from "./home.js";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Menu />
-      <Conceptos />
+      <div className="container mx-auto flex justify-center items-center w-full">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />{" "}
+            <Route path="/conceptos" element={<Conceptos />} />{" "}
+            <Route path="/alumnos" element={<Alumnos />} />{" "}
+          </Routes>{" "}
+        </BrowserRouter>{" "}
+      </div>
     </div>
   );
 }
--- src/Menu.js
+++ src/Menu.js
@@ -9,17 +9,34 @@ function Menu() {
   };
 
   return (
-    <nav className="navbar">
-      <div className="navbar-container">
-        <div className="navbar-brand"> 
-        <h3>PARCIAL 1</h3>
+    <div>
+      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+          <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
+            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Administración de Proyectos de TI</span>
+          </a>
+
         </div>
-        <ul className={menuOpen ? "menu-links active" : "menu-links"}>
-          <li><a href="./conceptos">Sección de Conceptos</a></li>
-          <li><a href="./alumnos">Seccion de Alumnos</a></li>
-        </ul>
-      </div>
-    </nav>
+      </nav>
+      <nav className="bg-gray-50 dark:bg-gray-700">
+        <div className="max-w-screen-xl px-4 py-3 mx-auto">
+          <div className="flex items-right">
+            <ul className="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
+              <li>
+                <a href="/" className="text-gray-900 dark:text-white hover:underline" aria-current="page">Home</a>
+              </li>
+              <li>
+                <a href="alumnos" className="text-gray-900 dark:text-white hover:underline">Sección de Alumnos</a>
+              </li>
+              <li>
+                <a href="conceptos" className="text-gray-900 dark:text-white hover:underline">Sección de Conceptos</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
   );
 }
 
